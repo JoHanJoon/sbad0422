e949ba46853b29637366fc11e909ef7383d3aa20
Hanjun Jo <hanjun.jo@cloudmt.co.kr>
2022-04-22 10:28:39 +0900

    first commit

---
+++ src/main/java/com/example/sba/sbad0422/Sbad0422Application.java
@@ -0,0 +1,13 @@
+package com.example.sba.sbad0422;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbad0422Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbad0422Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sba/sbad0422/Sbad0422ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sba.sbad0422;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbad0422ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
